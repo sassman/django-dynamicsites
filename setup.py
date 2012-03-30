fd2aff915efd25f07a60a02399117fce0b7bd627
Andrew Nelis <andrew.nelis@gmail.com>
2012-03-30 12:37:29 +0100

    More setup.py twiddling.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.core import setup
 setup(
     version='0.1',
     name='dynamicsites',
-    version=version,
     description="Host multiple sites from a single django project",
     url='https://bitbucket.org/uysrc/django-dynamicsites',
     platforms=['any'],
