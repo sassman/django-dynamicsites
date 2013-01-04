db3e5cc685362033df9044bb0c516a9faf725aeb
Sven Aßmann <sven.assmann@lubico.biz>
2013-01-04 19:54:56 +0100

    increment version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    version='0.1',
+    version='0.1.1',
     name='dynamicsites',
     description="Host multiple sites from a single django project",
     url='https://github.com/lubico-business/django-dynamicsites',
